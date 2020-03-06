a0256718b5520c700667c26932675347d9ed07dc
HoJin9622 <kiss0104040@gmail.com>
2020-03-06 22:46:49 +0900

    노드 리액트 기초 강의 #13 Auth 기능 만들기

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const port = 5000;
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const config = require("./config/key");
+const { auth } = require("./middleware/auth");
 const { User } = require("./models/User");
 
 // application/x-www-form-urlencoded
@@ -26,7 +27,7 @@ mongoose
 
 app.get("/", (req, res) => res.send("Hello World!~~"));
 
-app.post("/register", (req, res) => {
+app.post("/api/users/register", (req, res) => {
   //회원 가입 할때 필요한 정보들을 client에서 가져오면
   //그것들을 데이터베이스에 넣어준다.
   const user = new User(req.body); //body-parser가 있어서 req.body안에 id, pw 같은 정보를 넣음
@@ -36,7 +37,7 @@ app.post("/register", (req, res) => {
   });
 });
 
-app.post("/login", (req, res) => {
+app.post("/api/users/login", (req, res) => {
   // 요청된 이메일을 데이터베이스에서 찾는다.
   User.findOne({ email: req.body.email }, (err, user) => {
     if (!user) {
@@ -63,6 +64,23 @@ app.post("/login", (req, res) => {
       });
     });
   });
+});
+
+app.get("/api/users/auth", auth, (req, res) => {
+  // auth라는 미들웨어를 추가한다.
+  // 여기까지 미들웨어를 통과해 왔다는 애기는 Authentication 이 True라는 말.
+  res
+    .status(200)
+    .json({
+      _id: req.user._id,
+      isAdmin: req.user.role === 0 ? false : true,
+      isAuth: true,
+      email: req.user.email,
+      name: req.user.name,
+      latname: req.user.lastname,
+      role: req.user.role,
+      image: req.user.image
+    });
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
