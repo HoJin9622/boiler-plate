4c32bd719061c543d1a9573e1c4d5dd521cf1619
HoJin9622 <kiss0104040@gmail.com>
2020-03-16 20:11:28 +0900

    노드 리액트 기초 강의 #29 로그인 페이지(1)

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -6,7 +6,19 @@ function LandingPage() {
     axios.get("/api/hello").then(response => console.log(response)); // http://localhost:5000/api/hello 에 localhost를 빼줘야함
   }, []);
 
-  return <div>LandingPage</div>;
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        width: "100%",
+        height: "100vh"
+      }}
+    >
+      <h2>시작페이지</h2>
+    </div>
+  );
 }
 
 export default LandingPage;
