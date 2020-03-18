464c21aea2029e4085388bf51834fedc44e55238
HoJin9622 <kiss0104040@gmail.com>
2020-03-18 22:13:18 +0900

    노드 리액트 기초 강의 #32 로그아웃

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,10 +1,22 @@
 import React, { useEffect } from "react";
 import axios from "axios";
+import { PromiseProvider } from "mongoose";
 
-function LandingPage() {
+function LandingPage(props) {
   useEffect(() => {
     axios.get("/api/hello").then(response => console.log(response)); // http://localhost:5000/api/hello 에 localhost를 빼줘야함
   }, []);
+
+  const onClickHandler = () => {
+    axios.get(`/api/users/logout`).then(response => {
+      console.log(response.data);
+      if (response.data.success) {
+        props.history.push("/login");
+      } else {
+        alert("로그아웃 실패");
+      }
+    });
+  };
 
   return (
     <div
@@ -17,6 +29,8 @@ function LandingPage() {
       }}
     >
       <h2>시작페이지</h2>
+
+      <button onClick={onClickHandler}>로그아웃</button>
     </div>
   );
 }
